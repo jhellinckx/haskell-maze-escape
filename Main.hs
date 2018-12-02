8f2ee01a040ae3ff5aa9aa923a602a9bdebf763f
Jérôme Hellinckx <jerome.hellinckx@famoco.com>
2018-12-02 05:18:10 +0100

    [add] Replaced List with Array

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified System.Environment
 import Data.List
 import qualified Data.Map as Map
+import Data.Array
 
 main :: IO ()
 main = do [path] <- System.Environment.getArgs 
@@ -10,15 +11,27 @@ main = do [path] <- System.Environment.getArgs
           putStr $ unlines $ escape $ lines maze
 
 data MazeCell = Wall | Pathway | Path | Start | End deriving (Eq, Ord)
-type Maze = [[MazeCell]]
-type Coord = (Int, Int)
-type Path = [Coord]
+type Position = (Int, Int)
+type Path = [Position]
+type Maze = Array Position MazeCell
+
+motions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+cells = [(Wall, 'X'), (Pathway, ' '), (Path, '.'), (Start, '*'), (End, '@')]
+(serialize, deserialize) = bimappers cells
 
 escape :: [[Char]] -> [[Char]]
-escape maze = showPath dmaze (bfs dmaze)
-    where dmaze = map (map deserializeCell) maze
+escape maze = case bfs (mazeArray maze) of Nothing -> ["No solution found."]
+                                           Just solution -> map (map (markPath solution)) $ withPositions maze
 
-(serializeCell, deserializeCell) = bimappers [(Wall, 'X'), (Pathway, ' '), (Path, '.'), (Start, '*'), (End, '@')]
+markPath :: Path -> (Position, Char) -> Char
+markPath path (pos, c) = if elem pos path then serialize Path else c
+
+mazeArray :: [[Char]] -> Maze
+mazeArray maze = array mazeBounds $ (concat . withPositions . map (map deserialize)) maze 
+    where mazeBounds = ((0, 0), (length maze - 1, length (head maze) - 1))
+
+withPositions :: [[a]] -> [[(Position, a)]]
+withPositions matrix = map (\(i, row) -> zipWith (\j e -> ((i, j), e)) [0..] row) $ zip [0..] matrix
 
 bimappers :: (Ord a, Ord b) => [(a, b)] -> (a -> b, b -> a)
 bimappers keys = (mapper $ Map.fromList keys, mapper $ Map.fromList $ map swap keys) where swap (a, b) = (b, a)
@@ -26,48 +39,28 @@ bimappers keys = (mapper $ Map.fromList keys, mapper $ Map.fromList $ map swap k
 mapper :: (Ord a) => Map.Map a b -> a -> b
 mapper m k = m Map.! k
 
-showPath :: Maze -> Maybe Path -> [[Char]]
-showPath maze Nothing = ["No solution found."]
-showPath maze (Just path) = map (map serializeCell) $ map (map transform) $ withCoords maze
-    where transform (_, Start) = Start
-          transform (_, End) = End
-          transform ((x, y), cell) = if elem (x, y) path then Path else cell
-
-
-withCoords :: Maze -> [[(Coord, MazeCell)]]
-withCoords maze = map (\(y, row) -> zipWith (\x e -> ((x, y), e)) [0..] row) $ zip [0..] maze
-
-move :: (Int, Int) -> Coord -> Coord
+move :: (Int, Int) -> Position -> Position
 move (a, b) (x, y) = (x + a, y + b)
 
-motions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+adjacents :: Maze -> Position -> [Position]
+adjacents maze pos = (pathway . inside) candidates
+    where candidates = map move motions <*> (pure pos)
+          inside = filter (\(i, j) -> inRange (bounds maze) pos)
+          pathway = filter (\pos -> not (maze ! pos == Wall))
 
-adjacents :: Maze -> Coord -> [Coord]
-adjacents maze coord = (pathway . inside) candidates
-    where candidates = map move motions <*> (pure coord)
-          (bx, by) = ((0, length (maze !! 0)), (0, length maze))
-          inside = filter (\(x, y) -> inbounds bx x && inbounds by y)
-          pathway = filter (\coord -> not (maze `cell` coord == Wall))
-
-inbounds :: (Int, Int) -> Int -> Bool
-inbounds (lower, upper) x = lower <= x && x <= upper
-
-cell :: Maze -> Coord -> MazeCell
-maze `cell` (x, y) = maze !! y !! x
-
-coord :: Maze -> MazeCell -> Maybe Coord
-maze `coord` cell = pure fst <*> (find (\((x, y), c) -> c == cell) $ concat $ withCoords maze)
+position :: Maze -> MazeCell -> Maybe Position
+maze `position` cell = pure fst <*> (find (\((i, j), e) -> e == cell) $ assocs maze)
 
 bfs :: Maze -> Maybe Path
-bfs maze = maze `coord` Start >>= (bfstart maze)
+bfs maze = maze `position` Start >>= (bfstart maze)
 
-bfstart :: Maze -> Coord -> Maybe Path
-bfstart maze start = bfstep maze [(start, Start, [])] []
+bfstart :: Maze -> Position -> Maybe Path
+bfstart maze start = bfstep maze [(start, Start, [])] [start]
 
-bfstep :: Maze -> [(Coord, MazeCell, Path)] -> [Coord] -> Maybe Path
+bfstep :: Maze -> [(Position, MazeCell, Path)] -> [Position] -> Maybe Path
 bfstep _ [] _ = Nothing
-bfstep _ ((h, End, p):_) _ = Just (h:p)
-bfstep maze ((h, _, p):queue) visited = bfstep maze (queue ++ (map (\a -> (a, maze `cell` a, h:p)) adj)) (h:visited)
+bfstep _ ((_, End, _:p):_) _ = Just p
+bfstep maze ((h, _, p):queue) visited = bfstep maze (queue ++ (map (\pos -> (pos, maze ! pos, pos:p)) adj)) (visited ++ adj)
     where adj = filter (not . (flip elem visited)) (adjacents maze h)
 
 
